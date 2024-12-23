d863d61d5d6a8393754b88fd0f56f85ee299e3c4
AmitBarlev <Barlev.amitush@gmail.com>
2024-12-23 23:10:11 +0200

    INITILIZR

---
+++ src/test/java/com/abl/lmd/LmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abl.lmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
